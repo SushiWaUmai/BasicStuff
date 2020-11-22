6b16ec425691bac84c01da92c6ba62e081981738
SushiWaUmai <eugene.bb1@gmail.com>
2020-11-22 02:20:10 +0100

    Update BasicRigidbody2DExtension.cs

--- Runtime/Scripts/Unity/BasicRigidbody2DExtension.cs
+++ Runtime/Scripts/Unity/BasicRigidbody2DExtension.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class Rigidbody2DExtension
 {
-    public static void AddExplosionForce(this Rigidbody2D rb, float explosionForce, Vector2 explosionPosition, float explosionRadius, float upwardsModifier = 0.0F, ForceMode2D mode = ForceMode2D.Force)
+    public static void AddExplosionForce(this Rigidbody2D rb, float explosionForce, Vector2 explosionPosition, float explosionRadius, float upwardsModifier = 0.0F, ForceMode2D mode = ForceMode2D.Impulse)
     {
         var explosionDir = rb.position - explosionPosition;
         var explosionDistance = explosionDir.magnitude;
